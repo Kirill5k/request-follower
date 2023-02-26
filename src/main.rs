2f79082d9d9061f43552367e67a9e069b80bc13e
kirill5k <immotional@aol.com>
2023-02-26 13:32:17 +0000

    Clean routes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 use warp::Filter;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AppStatus {
+    status: String
+}
+
+impl AppStatus {
+    fn up() -> AppStatus {
+        AppStatus {
+            status: String::from("up")
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String)
+    let hello = warp::get()
+        .and(warp::path!("hello" / String))
         .map(|name| format!("Hello, {}!", name));
 
-    let health = warp::path!("health" / "status")
+    let health = warp::get()
+        .and(warp::path!("health" / "status"))
         .map(|| r#"{"status":"up"}"#);
 
-    let routes = warp::get().and(hello.or(health));
+    let routes = hello.or(health);
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
