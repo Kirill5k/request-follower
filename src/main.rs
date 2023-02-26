d9cc3fe7d7dad5f908c0ced71ad54e438ce3b16e
kirill5k <immotional@aol.com>
2023-02-26 13:39:02 +0000

    Return json response

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,22 @@ impl AppStatus {
 async fn main() {
     let hello = warp::get()
         .and(warp::path!("hello" / String))
+        .and(warp::path::end())
         .map(|name| format!("Hello, {}!", name));
 
     let health = warp::get()
         .and(warp::path!("health" / "status"))
-        .map(|| r#"{"status":"up"}"#);
+        .and(warp::path::end())
+        .and_then(get_health_status);
 
     let routes = hello.or(health);
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
+}
+
+async fn get_health_status() -> Result<impl warp::Reply, warp::Rejection> {
+    let status = AppStatus::up();
+    Ok(warp::reply::json(&status))
 }
