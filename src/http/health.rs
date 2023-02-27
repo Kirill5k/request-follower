432efa4d9ce3c2c1907f21cfb883aa0d09efc8f2
kirill5k <immotional@aol.com>
2023-02-27 12:00:53 +0000

    Add startup time to AppStatus

--- src/http/health.rs
+++ src/http/health.rs
@@ -1,27 +1,32 @@
 use warp::{Filter, Rejection, Reply};
 use serde::{Serialize, Deserialize};
+use time::OffsetDateTime;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct AppStatus {
-    status: String
+    status: String,
+    #[serde(with = "time::serde::rfc3339")]
+    startup_time: OffsetDateTime
 }
 
 impl AppStatus {
-    fn up() -> AppStatus {
+    fn up(startup_time: OffsetDateTime) -> AppStatus {
         AppStatus {
-            status: String::from("up")
+            status: String::from("up"),
+            startup_time
         }
     }
 }
 
-async fn get_health_status() -> Result<impl Reply, Rejection> {
-    let status = AppStatus::up();
+async fn get_health_status(startup_time: OffsetDateTime) -> Result<impl Reply, Rejection> {
+    let status = AppStatus::up(startup_time);
     Ok(warp::reply::json(&status))
 }
 
 pub fn routes() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
+    let startup_time = OffsetDateTime::now_utc();
     warp::get()
         .and(warp::path!("health" / "status"))
         .and(warp::path::end())
-        .and_then(get_health_status)
+        .and_then(move || get_health_status(startup_time))
 }
