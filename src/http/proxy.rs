8315dfb24751776fee25d4b127e329e11ad1cdd9
kirill5k <immotional@aol.com>
2023-02-28 12:42:48 +0000

    Extract more data from request

--- src/http/proxy.rs
+++ src/http/proxy.rs
@@ -1,15 +1,14 @@
 use warp::http::{HeaderMap, Method};
 use warp::{Filter, Rejection, Reply};
+use warp::path::FullPath;
+use std::collections::HashMap;
 
-pub fn routes() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
-    warp::path!("hello" / String)
-        .and(warp::path::end())
-        .and(warp::method())
+pub fn routes() -> impl Filter<Extract=(impl Reply, ), Error=Rejection> + Clone {
+    warp::method()
+        .and(warp::path::full())
+        .and(warp::query::<HashMap<String, String>>())
         .and(warp::header::headers_cloned())
-        .map(|name: String, method: Method, headers: HeaderMap| {
-            format!(
-                "Hello, {}!\nMethod: {}\nHeaders: {:?}",
-                name, method, headers
-            )
+        .map(|method: Method, full_path: FullPath, query: HashMap<String, String>, headers: HeaderMap| {
+            format!("{method} {:?} {:?}\n{:?}", full_path, query, headers)
         })
 }
