6757d48e69dab96c1ce1adac3cda1b49b8d02406
kirill5k <immotional@aol.com>
2023-03-03 10:06:37 +0000

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ async fn main() {
     env_logger::init();
     info!("starting request-follower");
 
-    let routes = health::routes().or(proxy::routes()).with(warp::log("request_follower"));
+    let routes = health::routes()
+        .or(proxy::routes())
+        .with(warp::log("request_follower"));
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
