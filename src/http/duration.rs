2add4c085879291718ce4cf24325a388f7a5e2f1
kirill5k <immotional@aol.com>
2023-05-15 14:00:17 +0100

    Implement subtract

--- src/http/duration.rs
+++ src/http/duration.rs
@@ -1,10 +1,24 @@
 use time::{Duration, OffsetDateTime};
+use std::ops;
 
 pub struct FiniteDuration {
     seconds: i64
 }
 
 impl FiniteDuration {
+    pub fn from_seconds(seconds: i64) -> FiniteDuration {
+        FiniteDuration { seconds }
+    }
+
+    pub fn from(d: Duration) -> FiniteDuration {
+        let seconds = d.whole_seconds();
+        FiniteDuration { seconds }
+    }
+
+    pub fn between_now_and(other_date: OffsetDateTime) -> FiniteDuration {
+        FiniteDuration::from(OffsetDateTime::now_utc() - other_date)
+    }
+
     pub fn to_days(&self) -> i64 {
         self.seconds / 86400
     }
@@ -38,17 +52,18 @@ impl FiniteDuration {
         }
     }
 
-    pub fn from(d: Duration) -> FiniteDuration {
-        let seconds = d.whole_seconds();
-        FiniteDuration { seconds }
-    }
-
-    pub fn between_now_and(other_date: OffsetDateTime) -> FiniteDuration {
-        FiniteDuration::from(OffsetDateTime::now_utc() - other_date)
-    }
-
     pub fn to_string(&self) -> String {
         String::from("")
+    }
+}
+
+impl ops::Sub<FiniteDuration> for FiniteDuration {
+    type Output = FiniteDuration;
+
+    fn sub(self, other: FiniteDuration) -> Self::Output {
+        FiniteDuration {
+            seconds: (self.seconds - other.seconds).abs()
+        }
     }
 }
 
@@ -67,5 +82,14 @@ mod tests {
         assert_eq!(129600, finite_duration.minutes().seconds);
         assert_eq!(129600, finite_duration.hours().seconds);
         assert_eq!(86400, finite_duration.days().seconds);
+    }
+
+    #[test]
+    fn subtract() {
+        let fd_1 = FiniteDuration::from_seconds(3160);
+        let fd_2 = FiniteDuration::from_seconds(3000);
+        let result = fd_1 - fd_2;
+
+        assert_eq!(160, result.seconds)
     }
 }
